bcc39e2eda9fc25aa66b4f44ecf79d8163ac1222
Mohamed Makhtout <medmakhtout@gmail.com>
2026-03-29 21:54:35 +0100

    petite opt

--- src/main/java/com/vogdo/Main.java
+++ src/main/java/com/vogdo/Main.java
@@ -1,14 +1,35 @@
 package com.vogdo;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Context context = new Context();
-        context.effectuerOperation();
+        /*context.effectuerOperation();
         context.setStrategy(new StrategyImpl1());
         context.effectuerOperation();
         context.setStrategy(new StrategyImpl2());
         context.effectuerOperation();
         context.setStrategy(new StrategyImpl3());
-        context.effectuerOperation();
+        context.effectuerOperation();*/
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String,Strategy> strategyMap = new HashMap<>();
+        Strategy strategy;
+        while(true){
+            System.out.print("Quelle strategy: ");
+            String strategyName = scanner.nextLine();
+            strategy = strategyMap.get(strategyName);
+            if(strategy == null){
+                System.out.println("Creation d'un vouvel objet de StrategyImpl"+strategyName);
+                strategy=(Strategy) Class.forName("com.vogdo.StrategyImpl"+strategyName).getConstructor().newInstance();
+                strategyMap.put(strategyName,strategy);
+            }
+            context.setStrategy(strategy);
+            context.effectuerOperation();
+        }
     }
 }
